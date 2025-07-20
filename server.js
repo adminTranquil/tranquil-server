b87b74de7848932ef747bfd79c433e10529a0254
mayodi3 <mayodisaac3@gmail.com>
2025-07-20 12:22:55 +0300

    Fixed cors issues

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://thetranquilhospitalsltd.co.ke"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
